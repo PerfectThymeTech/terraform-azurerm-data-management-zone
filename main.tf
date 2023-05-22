7a0d73cd88d7e1f2c85ff4890d1bdfb85a8a7ba5
Marvin Buss <marvin.buss@gmail.com>
2023-05-22 20:59:10 +0200

    Initial push

---
+++ main.tf
@@ -0,0 +1,31 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "governance_rg" {
+  name     = "${local.prefix}-governance-rg"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "unity_rg" {
+  name     = "${local.prefix}-unity-rg"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "container_rg" {
+  name     = "${local.prefix}-container-rg"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "consumption_rg" {
+  name     = "${local.prefix}-consumption-rg"
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_resource_group" "automation_rg" {
+  name     = "${local.prefix}-automation-rg"
+  location = var.location
+  tags     = var.tags
+}
---
+++ terraform.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">=0.13"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.56.0"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = ">= 1.6.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = ">= 1.16.0"
+    }
+  }
+}
+
+provider "databricks" {
+  azure_environment           = "public"
+  azure_workspace_resource_id = azurerm_databricks_workspace.databricks.id
+  host                        = azurerm_databricks_workspace.databricks.workspace_url
+}
