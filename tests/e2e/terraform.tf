55e0b10a2031eb29efe043a2aa4d00df353942f8
Marvin Buss <marvin.buss@gmail.com>
2023-05-24 18:22:19 +0200

    Update version constraints

--- tests/e2e/terraform.tf
+++ tests/e2e/terraform.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.56.0"
+      version = "3.57.0"
     }
     azapi = {
       source  = "azure/azapi"
@@ -12,7 +12,7 @@ terraform {
     }
     databricks = {
       source  = "databricks/databricks"
-      version = "1.16.0"
+      version = "1.16.1"
     }
   }
 
