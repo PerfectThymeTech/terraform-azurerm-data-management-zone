a0372415ca64ad9d8c2a11da0fced2789208ee48
Marvin Buss <marvin.buss@gmail.com>
2023-05-25 10:21:17 +0200

    Update tf module requirements

--- terraform.tf
+++ terraform.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.56"
+      version = ">= 3.56.0"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "~> 1.5"
+      version = ">= 1.5.0"
     }
     databricks = {
       source  = "databricks/databricks"
-      version = "~> 1.16"
+      version = ">= 1.16.0"
     }
   }
 }
