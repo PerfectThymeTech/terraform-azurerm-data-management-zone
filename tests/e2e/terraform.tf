0975381dd8b1b82f847fd1fb2de4142f06408c50
Marvin Buss <marvin.buss@gmail.com>
2024-11-18 13:00:35 +0100

    Update tf reference

--- tests/e2e/terraform.tf
+++ tests/e2e/terraform.tf
@@ -4,25 +4,28 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.76.0"
+      version = "4.10.0"
     }
     azapi = {
       source  = "azure/azapi"
-      version = "1.9.0"
+      version = "2.0.1"
     }
     databricks = {
       source  = "databricks/databricks"
-      version = "1.24.0"
+      version = "1.58.0"
+    }
+    time = {
+      source = "hashicorp/time"
+      version = "0.12.1"
     }
   }
 
   backend "azurerm" {
     environment          = "public"
-    resource_group_name  = "mycrp-prd-cicd"
-    storage_account_name = "mycrpprdstg001"
-    container_name       = "data-management-zone"
-    key                  = "terraform.tfstate"
+    resource_group_name  = "<provided-via-config>"
+    storage_account_name = "<provided-via-config>"
+    container_name       = "<provided-via-config>"
+    key                  = "<provided-via-config>"
     use_azuread_auth     = true
-    # use_oidc             = true
   }
 }
