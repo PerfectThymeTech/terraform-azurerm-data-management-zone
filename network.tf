907bace291a9f5f4f004280efc72e18892044f48
Marvin Buss <marvin.buss@gmail.com>
2024-11-19 17:09:16 +0100

    Test different subnet structure

--- network.tf
+++ network.tf
@@ -32,48 +32,46 @@ resource "azurerm_virtual_network" "virtual_network_databricks" {
 
   address_space = ["10.0.0.0/20"]
   dns_servers   = []
-  subnet = [
-    {
-      name                            = local.databricks_private_subnet_name
-      address_prefixes                = ["10.0.0.0/26"]
-      default_outbound_access_enabled = false
-      delegation = [
-        {
-          name = "Microsoft.Databricks/workspaces"
-          actions = [
-            "Microsoft.Network/virtualNetworks/subnets/join/action",
-            "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
-            "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action"
-          ]
-        }
-      ]
-      private_endpoint_network_policies             = "Enabled"
-      private_link_service_network_policies_enabled = "Enabled"
-      route_table_id                                = data.azurerm_route_table.route_table.id
-      security_group                                = data.azurerm_network_security_group.network_security_group.id
-      service_endpoint_policy_ids                   = []
-      service_endpoints                             = []
-    },
-    {
-      name                            = local.databricks_public_subnet_name
-      address_prefixes                = ["10.0.0.64/26"]
-      default_outbound_access_enabled = false
-      delegation = [
-        {
-          name = "Microsoft.Databricks/workspaces"
-          actions = [
-            "Microsoft.Network/virtualNetworks/subnets/join/action",
-            "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
-            "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action"
-          ]
-        }
-      ]
-      private_endpoint_network_policies             = "Enabled"
-      private_link_service_network_policies_enabled = "Enabled"
-      route_table_id                                = data.azurerm_route_table.route_table.id
-      security_group                                = data.azurerm_network_security_group.network_security_group.id
-      service_endpoint_policy_ids                   = []
-      service_endpoints                             = []
-    }
-  ]
+  subnet {
+    name                            = local.databricks_private_subnet_name
+    address_prefixes                = ["10.0.0.0/26"]
+    default_outbound_access_enabled = false
+    delegation = [
+      {
+        name = "Microsoft.Databricks/workspaces"
+        actions = [
+          "Microsoft.Network/virtualNetworks/subnets/join/action",
+          "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
+          "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action"
+        ]
+      }
+    ]
+    private_endpoint_network_policies             = "Enabled"
+    private_link_service_network_policies_enabled = "Enabled"
+    route_table_id                                = data.azurerm_route_table.route_table.id
+    security_group                                = data.azurerm_network_security_group.network_security_group.id
+    service_endpoint_policy_ids                   = []
+    service_endpoints                             = []
+  }
+  subnet {
+    name                            = local.databricks_public_subnet_name
+    address_prefixes                = ["10.0.0.64/26"]
+    default_outbound_access_enabled = false
+    delegation = [
+      {
+        name = "Microsoft.Databricks/workspaces"
+        actions = [
+          "Microsoft.Network/virtualNetworks/subnets/join/action",
+          "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
+          "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action"
+        ]
+      }
+    ]
+    private_endpoint_network_policies             = "Enabled"
+    private_link_service_network_policies_enabled = "Enabled"
+    route_table_id                                = data.azurerm_route_table.route_table.id
+    security_group                                = data.azurerm_network_security_group.network_security_group.id
+    service_endpoint_policy_ids                   = []
+    service_endpoints                             = []
+  }
 }
