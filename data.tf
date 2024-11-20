f2b0fa7a02856946fa4ceed58b2f2c98544a7f8a
Marvin Buss <marvin.buss@gmail.com>
2024-11-20 12:04:18 +0100

    Fetch secret via data object

--- data.tf
+++ data.tf
@@ -12,3 +12,12 @@ data "azurerm_route_table" "route_table" {
   name                = local.route_table.name
   resource_group_name = local.route_table.resource_group_name
 }
+
+data "azurerm_key_vault_secret" "key_vault_secret_databricks_account_scim_token" {
+  key_vault_id = module.key_vault_scim.key_vault_id
+  name         = local.databricks_account_scim_secret_name
+
+  depends_on = [
+    null_resource.databricks_account_scim_token,
+  ]
+}
