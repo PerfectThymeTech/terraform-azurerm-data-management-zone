39f53aa8d1071968068051b53031b20ffc887d71
Marvin Buss <marvin.buss@gmail.com>
2024-11-20 18:49:10 +0100

    Add Databricks Account Setting Configuration

---
+++ databricksaccountsettings.tf
@@ -0,0 +1,17 @@
+resource "null_resource" "databricks_account_settings" {
+  triggers = {
+    company_name      = var.company_name
+    enable_serverless = true
+    trigger_id        = 2
+  }
+
+  provisioner "local-exec" {
+    working_dir = "${path.module}/scripts/"
+    command     = "pwsh ./Set-AccountSettings.ps1 -DatabricksAccountId ${var.databricks_account_id} -CompanyName ${var.company_name} -EnableServerless true"
+    environment = {}
+  }
+
+  depends_on = [
+    module.key_vault_scim.key_vault_setup_completed
+  ]
+}
