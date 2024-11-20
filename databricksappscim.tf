11076154727eb9bd3b42493311088edebf5c7897
Marvin Buss <marvin.buss@gmail.com>
2024-11-20 12:23:11 +0100

    Update script reference

--- databricksappscim.tf
+++ databricksappscim.tf
@@ -9,7 +9,7 @@ resource "null_resource" "databricks_account_scim_token" {
 
   provisioner "local-exec" {
     working_dir = "${path.module}/scripts/"
-    command     = "pwsh ./manage-synapse-mpes.sh -DatabricksAccountId ${var.databricks_account_id} -KeyVaultName ${module.key_vault_scim.key_vault_name} -KeyVaultSecretName ${local.databricks_account_scim_secret_name}"
+    command     = "pwsh ./Get-AccountScimToken.ps1 -DatabricksAccountId ${var.databricks_account_id} -KeyVaultName ${module.key_vault_scim.key_vault_name} -KeyVaultSecretName ${local.databricks_account_scim_secret_name}"
     environment = {}
   }
 
