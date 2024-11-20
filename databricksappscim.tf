31de096c88148fbfb4e5892c7b1599ab67c37ced
Marvin Buss <marvin.buss@gmail.com>
2024-11-20 12:04:08 +0100

    Add scim secret creation

--- databricksappscim.tf
+++ databricksappscim.tf
@@ -1,6 +1,19 @@
-data "external" "databricks_access_token" {
-  program = [
-    "pwsh", "${path.module}/scripts/Get-Token.ps1", "-Resource", "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d"
+resource "time_rotating" "databricks_account_scim_token_regenerate" {
+  rotation_months = 1
+}
+
+resource "null_resource" "databricks_account_scim_token" {
+  triggers = {
+    execute = time_rotating.databricks_account_scim_token_regenerate.rfc3339
+  }
+
+  provisioner "local-exec" {
+    working_dir = "${path.module}/scripts/"
+    command     = "pwsh ./manage-synapse-mpes.sh -DatabricksAccountId ${var.databricks_account_id} -KeyVaultName ${module.key_vault_scim.key_vault_name} -KeyVaultSecretName ${local.databricks_account_scim_secret_name}"
+    environment = {}
+  }
+
+  depends_on = [
+    module.key_vault_scim.key_vault_setup_completed
   ]
-  query = {}
 }
