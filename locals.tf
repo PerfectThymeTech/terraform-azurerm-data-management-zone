f006ea8893e7833f67f6409b105ee2f9b21a11d3
Marvin Buss <marvin.buss@gmail.com>
2024-12-17 12:39:25 +0100

    Redesign root module

--- locals.tf
+++ locals.tf
@@ -1,35 +1,18 @@
 locals {
+  # General locals
   prefix = "${lower(var.prefix)}-${var.environment}"
 
-  # General locals
+  # Diagnostics locals
   diagnostics_configurations = var.log_analytics_workspace_id == null ? [] : [
     {
       log_analytics_workspace_id = var.log_analytics_workspace_id
       storage_account_id         = ""
     }
   ]
+
+  # Customer managed key locals
   customer_managed_key = null
 
   # Network locals
-  virtual_network = {
-    resource_group_name = split("/", var.vnet_id)[4]
-    name                = split("/", var.vnet_id)[8]
-  }
-  network_security_group = {
-    resource_group_name = try(split("/", var.nsg_id)[4], "")
-    name                = try(split("/", var.nsg_id)[8], "")
-  }
-  route_table = {
-    resource_group_name = try(split("/", var.route_table_id)[4], "")
-    name                = try(split("/", var.route_table_id)[8], "")
-  }
-  subnet_cidr_ranges = {
-    private_endpoint_subnet = var.subnet_cidr_ranges.private_endpoint_subnet != "" ? var.subnet_cidr_ranges.private_endpoint_subnet : tostring(cidrsubnet(data.azurerm_virtual_network.virtual_network.address_space[0], 27 - tonumber(reverse(split("/", data.azurerm_virtual_network.virtual_network.address_space[0]))[0]), 0))
-  }
   connectivity_delay_in_seconds  = 30
-  databricks_private_subnet_name = "DatabricksPrivateSubnet"
-  databricks_public_subnet_name  = "DatabricksPublicSubnet"
-
-  # Databricks locals
-  databricks_account_scim_secret_name = "scim-token"
 }
--- roleassignments.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_role_assignment" "purview_roleassignment_key_vault" {
-  scope                = module.key_vault_purview.key_vault_id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = module.purview_account.purview_account_principal_id
-}
