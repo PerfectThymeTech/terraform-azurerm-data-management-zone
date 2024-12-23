037941453d94715cafb969cde08c0a6c7ce51726
Marvin Buss <marvin.buss@gmail.com>
2024-12-23 09:30:12 +0100

    Update provider version

--- tests/e2e/terraform.tf
+++ tests/e2e/terraform.tf
@@ -12,7 +12,7 @@ terraform {
     }
     databricks = {
       source  = "databricks/databricks"
-      version = "1.61.0"
+      version = "1.62.0"
     }
     time = {
       source  = "hashicorp/time"
